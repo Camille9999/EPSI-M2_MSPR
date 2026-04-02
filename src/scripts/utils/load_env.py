0f135a1bd6253074c090f159fa6a1a76d84b3dd8
Camille HOAREAU <c.hoareau@groupeonepoint.com>
2026-04-02 15:28:18 +0200

    ADD - environment variable configuration and load_env script

---
+++ src/scripts/utils/load_env.py
@@ -0,0 +1,40 @@
+# Standard librairies
+import os
+
+# External packages
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+#################
+#### CHEMINS ####
+#################
+PATH_DATA = os.getenv('PATH_DATA')
+#### !CHEMINS ####
+
+
+
+##################
+#### FICHIERS ####
+##################
+
+#### !FICHIERS ####
+
+
+##################
+#### VARIABLE ####
+##################
+DATA_ANN_DEF_YR_START = int(os.getenv('DATA_ANN_DEF_YR_START', 2012))
+DATA_ANN_DEF_YR_END = int(os.getenv('DATA_ANN_DEF_YR_END', 2024))
+## Docker ##
+
+#### !VARIABLE ####
+
+
+###############
+#### LIENS ####
+###############
+
+#### !LIENS ####
